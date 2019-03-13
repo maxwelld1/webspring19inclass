1a4687c81c8558352be80de639d5b5362aa1760c
maxwelld1 <smashdadrum@gmail.com>
2019-03-13 11:33:45 -0400

    push previously untracked changes

---
+++ server/models/user.js
@@ -0,0 +1,37 @@
+const conn = require('./mysql_connection');
+
+const model = {
+    getAll(cb) {
+        conn.query("SELECT * FROM 2019Spring_Persons", (err, data) => {
+            cb(err, data);
+        });    
+    },
+    get(id, cb) {
+        conn.query("SELECT * FROM 2019Spring_Persons WHERE Id=?", id, (err, data) => {
+            cb(err, data[0]);
+        });   
+    },
+    add(input, cb) {
+        if(!input.Password) {
+            cb(Error('Password Required'));
+            return;
+        } else if(input.Password.length < 8) {
+            cb(Error('A longer password is required.'));
+            return;
+        }
+        conn.query("INSERT INTO 2019Spring_Persons (FirstName, LastName, Birthday, Password, created_at) VALUES (?)", 
+                [[input.FirstName, input.LastName, input.Birthday, input.Password, new Date()]], 
+                (err, data) => {
+                    if(err){
+                        cb(err);
+                        return;
+                    }
+                    model.get(data.insertId, (err, data) => {
+                        cb(err, data);
+                    })
+                }
+        );   
+    }
+};
+
+module.exports = model;
