84eab0bedfbe089770d53dff913248a85dcbf62b
maxwelld1 <smashdadrum@gmail.com>
2019-03-28 10:47:45 -0400

    add models to Vue client

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div>
+    <Nav />
+    <div class="container" id="main">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Nav from "@/components/Nav";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Nav
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
 </style>
---
+++ src/views/MyFriends.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1 v-if="Globals.user">{{Globals.user.name}}Friends Page</h1> <!-- user starts as null in api -->
+        <ul>
+            <li v-for="friend in friends" :key="friend.id">{{friend.FirstName}}}</li>               
+            <li>Friend 2</li>
+            <li>Friend 3</li>
+            <li>Friend 4</li>
+            <li>Friend 5</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { Globals } from "@/models/api";
+import { MyFriends } from "../models/users.js";
+
+GetFriends();
+
+export default {
+    data: () => ({
+        Globals: Globals,
+        friends: []
+    }),
+    async mounted(){
+        this.friends = await GetFriends();
+    }
+}
+</script>
+
+<style>
+
+</style>
